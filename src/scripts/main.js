90f5e97cc6067ae6a210a68a675cf717015aed3c
kirky359 <lol783176@gmail.com>
2025-08-26 20:29:50 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+const employees = Array.from(items).map((el) => ({
+  name: el.textContent.trim(),
+  position: el.dataset.position,
+  salary: Number(el.dataset.salary.replace(/[$,]/g, '')),
+}));
+
+const getEmployees = (list) => {
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  list.forEach((element) => {
+    const li = document.createElement('li');
+
+    li.textContent = element.name;
+    li.dataset.position = element.position;
+    li.dataset.salary = `$${element.salary.toLocaleString()}`;
+    ul.appendChild(li);
+  });
+};
+
+const sortList = (list) => {
+  return list.sort((a, b) => b.salary - a.salary);
+};
+
+getEmployees(sortList(employees));
